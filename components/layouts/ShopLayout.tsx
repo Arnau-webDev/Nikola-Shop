5025c7489cbeffa9dd5eb3b8735bd61b384c3dff
Arnau-webDev <arnauma16@gmail.com>
2023-02-14 20:58:44 +0100

    created shop-layout, mui theme and folder structure

---
+++ components/layouts/ShopLayout.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+
+interface Props {
+    title: string,
+    pageDescription: string,
+    imageFullUrl?: string,
+    children?: React.ReactNode
+}
+
+export const ShopLayout: React.FC<Props> = ({ title, pageDescription, imageFullUrl, children }) => {
+	return (
+		<>
+			<Head>
+				<title>{ title }</title>
+
+				<meta name='description' content={ pageDescription }/>
+				<meta name='og:title' content={ title }/>
+				<meta name='og:description' content={ pageDescription }/>
+
+				{
+					imageFullUrl && (
+						<meta name='og:image' content={ imageFullUrl }/>
+					)
+				}
+			</Head>
+
+			<nav>
+
+			</nav>
+
+			{}
+
+			<main style={{
+				margin: '80px auto',
+				maxWidth: '1440px',
+				padding: '0px 30px'
+			}}>
+				{children}
+			</main>
+
+			<footer>
+                
+			</footer>
+		</>
+	);
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import { lightTheme } from '../themes';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ThemeProvider theme={lightTheme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
 }
