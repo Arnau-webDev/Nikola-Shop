5faa0f54aebdf3e6b08e58908c1939f95ea2fac5
Arnau-webDev <arnauma16@gmail.com>
2023-04-01 23:01:25 +0200

    Frontend form validation using React Hook Form

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,38 +1,76 @@
 import NextLink from 'next/link';
-import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { AuthLayout } from '../../components/layouts';
 
+import { validations } from '@/utils';
+
+import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
+type FormData = {
+	email: string,
+	password: string,
+  };
+
 const LoginPage = () => {
+
+	const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+	const onLoginUser = (data: FormData) => {
+	};
+
 	return (
 		<AuthLayout title={'Login'}>
-			<Box sx={{ width: 350, padding:'10px 20px' }}>
-				<Grid container spacing={2}>
-					<Grid item xs={12}>
-						<Typography variant='h1' component="h1">Log In</Typography>
-					</Grid>
+			<form onSubmit={handleSubmit(onLoginUser)} noValidate>
+				<Box sx={{ width: 350, padding:'10px 20px' }}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<Typography variant='h1' component="h1">Log In</Typography>
+						</Grid>
 
-					<Grid item xs={12}>
-						<TextField label="Email" variant="filled" fullWidth />
-					</Grid>
-					<Grid item xs={12}>
-						<TextField label="Password" type='password' variant="filled" fullWidth />
-					</Grid>
+						<Grid item xs={12}>
+							<TextField 
+								label="Email" 
+								variant="filled" 
+								fullWidth 
+								{ ...register('email', {
+									required: 'This field is required',
+									validate: validations.isEmail
+								}) }
+								error={ !!errors.email}
+								helperText={errors.email?.message}
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField 
+								label="Password" 
+								type='password' 
+								variant="filled" 
+								fullWidth 
+								{ ...register('password', {
+									required: 'This field is required',
+									minLength: { value: 6, message: 'At least 6 characters needed'}
+								}) }
+								error={ !!errors.password}
+								helperText={errors.password?.message}
+							/>
+						</Grid>
 
-					<Grid item xs={12}>
-						<Button color="secondary" className='circular-btn' size='large' fullWidth>
+						<Grid item xs={12}>
+							<Button color="secondary" className='circular-btn' size='large' fullWidth type='submit'>
                         Log In
-						</Button>
-					</Grid>
+							</Button>
+						</Grid>
 
-					<Grid item xs={12} display='flex' justifyContent='center'>
-						<NextLink href="/auth/register" passHref legacyBehavior>
-							<Link underline='always' color='secondary'>
-								{'¿Don\'t have an account?'}
-							</Link>
-						</NextLink>
+						<Grid item xs={12} display='flex' justifyContent='center'>
+							<NextLink href="/auth/register" passHref legacyBehavior>
+								<Link underline='always' color='secondary'>
+									{'¿Don\'t have an account?'}
+								</Link>
+							</NextLink>
+						</Grid>
 					</Grid>
-				</Grid>
-			</Box>
+				</Box>
+			</form>
 		</AuthLayout>
 	);
 };
