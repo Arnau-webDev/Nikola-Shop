0ba1092a2088af1b1854483275111988c35b52d3
Arnau-webDev <arnauma16@gmail.com>
2023-05-15 12:36:12 +0200

    Paypal verify OAuth token from our backend

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 type Data = {
     message: string
@@ -13,7 +14,37 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 	}
 }
 
-function payOrder(req: NextApiRequest, res: NextApiResponse<Data>) {
+const getPaypalBearerToken = async (): Promise<string | null> => {
+	const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+	const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
 
-	return res.status(200).json({ message: 'Order is paid :)' });
+	const base64Token = Buffer.from(`${ PAYPAL_CLIENT }:${ PAYPAL_SECRET }`, 'utf-8').toString('base64');
+	const body = new URLSearchParams('grant_type=client_credentials');
+
+	try {
+		const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+			headers: {
+				'Authorization': `Basic ${ base64Token }`,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		});
+
+		return data.access_token;
+	} catch (error) {
+		if(axios.isAxiosError(error)) {
+			console.log(error?.response?.data);
+		} else {
+			console.log(error);
+		}
+		return null;
+	}
+};
+
+async function payOrder(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+	const paypalBearerToken = await getPaypalBearerToken();
+
+	if(!paypalBearerToken) return res.status(400).json({ message: 'Could not confirm paypal token' }); 
+
+	return res.status(200).json({ message: paypalBearerToken });
 }
