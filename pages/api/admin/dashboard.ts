92af3a6b07a7c40d4d10afd51bf06f09f968014f
Arnau-webDev <arnauma16@gmail.com>
2023-05-16 22:53:44 +0200

    REST API get dashboard info

---
+++ pages/api/admin/dashboard.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Order, Product, User } from '../../../models';
+
+type Data = {
+    numberOfOrders: number;
+    paidOrders: number; // isPad true
+    notPaidOrders: number;
+    numberOfClients: number; // role: client
+    numberOfProducts: number;
+    productsWithNoInventory: number; // 0
+    lowInventory: number; // products with 10 or less
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+	await db.connect();
+    
+	const [
+		numberOfOrders,
+		paidOrders,
+		numberOfClients,
+		numberOfProducts,
+		productsWithNoInventory,
+		lowInventory,
+	] = await Promise.all([
+		Order.count(),
+		Order.find({ isPaid: true }).count(),
+		User.find({ role: 'client' }).count(),
+		Product.count(),
+		Product.find({ inStock: 0 }).count(),
+		Product.find({ inStock: { $lte: 10 } }).count(),
+	]);
+
+    
+	await db.disconnect();
+
+	res.status(200).json({
+		numberOfOrders,
+		paidOrders,
+		numberOfClients,
+		numberOfProducts,
+		productsWithNoInventory,
+		lowInventory,
+		notPaidOrders: numberOfOrders - paidOrders
+	});
+
+
+}
