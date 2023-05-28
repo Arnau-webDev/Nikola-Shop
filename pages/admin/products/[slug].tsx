2bbc8379dab97d9af2093f3d28e19aa0d8db5547
Arnau-webDev <arnauma16@gmail.com>
2023-05-28 20:20:25 +0200

    Handle checkbox with product info

--- pages/admin/products/[slug].tsx
+++ pages/admin/products/[slug].tsx
@@ -45,6 +45,16 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
 		// getValues('');
 	};
 
+	const onChnageSize = (size: string) => {
+		const currentSizes = getValues('sizes');
+
+		if(currentSizes.includes(size)) {
+			return setValue('sizes', currentSizes.filter((s) => s !== size), {shouldValidate: true});
+		}
+
+		setValue('sizes', [...currentSizes, size], {shouldValidate: true});
+	};
+
 	return (
 		<AdminLayout 
 			title={'Producto'} 
@@ -169,7 +179,12 @@ const ProductAdminPage:FC<Props> = ({ product }) => {
 							<FormLabel>Sizes</FormLabel>
 							{
 								validSizes.map(size => (
-									<FormControlLabel key={size} control={<Checkbox />} label={ size } />
+									<FormControlLabel 
+										key={size} 
+										control={<Checkbox  checked={getValues('sizes').includes(size)} /> } 
+										label={ size } 
+										onChange={() => onChnageSize(size)}
+									/>
 								))
 							}
 						</FormGroup>
