f84d89ec3b59cbf97fdc2f5871c50d6246faae3a
Arnau-webDev <arnauma16@gmail.com>
2023-05-29 22:17:04 +0200

    RESP API endpoint updateProduct

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -1,11 +1,13 @@
 import { db } from '@/database';
 import { IProduct } from '@/interfaces';
 import { Product } from '@/models';
+import { isValidObjectId } from 'mongoose';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = 
 | { message: string }
 | IProduct[]
+| IProduct
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
 
@@ -13,8 +15,7 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
 	case 'GET':
 		return getProducts(req, res);
 	case 'PUT':
-		// return updateProducts();
-		break;
+		return updateProduct(req, res);
 	case 'POST':
 		// return postProduct();
 		break;
@@ -32,6 +33,39 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 	// Need to update images
 
 	res.status(200).json(products);
+
+};
+
+const updateProduct = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+	const { _id = '', images = [] } = req.body as IProduct;
+
+	if( isValidObjectId(_id)) return res.status(400).json({ message: 'Product ID is not valid!'});
+
+	if(images.length <= 2) return res.status(400).json({ message: 'At least 2 images needed'});
+
+	// TODO: get images absolute path
+
+	try {
+		await db.connect();
+		const product = await Product.findById(_id);
+
+		if(!product) {
+			await db.disconnect();
+			return res.status(400).json({ message: 'No product exists with that ID'});
+		}
+
+		// TODO: delete photos in Cloudinary
+
+
+		await product.update(req.body);
+		await db.disconnect();
+
+		return res.status(200).json(product);
+	} catch (error) {
+		console.log(error);
+		await db.disconnect();
+		return res.status(400).json({ message: 'Check updateProducts server logs'});
+	}
 
 };
 
